22924ca89a17e2a31f013a33b89d2837985c1f0b
aljosa.osep <aljosa.osep@gmail.com>
2022-11-19 19:19:41 +0100

    A few changes in page gen script

--- generator_utils.py
+++ generator_utils.py
@@ -2,6 +2,23 @@
 import json
 from nameparser import HumanName
 
+def get_about_from_json(json_file):
+	about = None
+	with open(json_file) as json_file:
+		data = json.load(json_file)
+		about = data["about"]
+	return about
+
+	# 	for item in news:
+	# 		if "html" not in item:
+	# 			raise ValueError("Yo need html to gen stuff!")
+	# 		if "date" in item:
+	# 			gen_str += "<li>[{}] {}</li>".format(item["date"], item["html"])
+	# 		else:
+	# 			gen_str += "<li>{}</li>".format(item["html"])
+	# gen_str += "</ul>"
+	# return gen_str
+
 def gen_timestamped_list_from_json(json_file, source):
 	gen_str = "<ul>"
 	with open(json_file) as json_file:
@@ -11,7 +28,7 @@ def gen_timestamped_list_from_json(json_file, source):
 			if "html" not in item:
 				raise ValueError("Yo need html to gen stuff!")
 			if "date" in item:
-				gen_str += "<li>[{}] {}</li>".format(item["date"], item["html"])
+				gen_str += "<li><b><i>{}:</i></b> {}</li>".format(item["date"], item["html"])
 			else:
 				gen_str += "<li>{}</li>".format(item["html"])
 	gen_str += "</ul>"
--- page_gen.py
+++ page_gen.py
@@ -4,15 +4,17 @@ from pylatexenc.latex2text import LatexNodes2Text
 
 import generator_utils as gut
 
+# === News ===
+desc = gut.get_about_from_json(json_file='content.json') 
 
 
 # Most basics stuff
 caption = """Aljosa's Web Corner"""
 title = """Aljosa Osep, Ph.D."""
-desc = """Hi, I'm Aljosa! I am working towards scaling object detection, segmentation, and tracking models to the open world and learning from raw, unlabeled streams of sensory data.  
-	<br><br>
-	I come from the Alpine side of Slovenia. I have obtained my M.Sc. degree at the University of Bonn and Ph.D. at RWTH Aachen University under the supervision of <a href="https://www.vision.rwth-aachen.de/person/1/">Prof. Dr. Bastian Leibe</a>. 
-	Currently, I am working with <a href="http://www.cs.cmu.edu/~deva/">Prof. Deva Ramanan</a> at the CMU Robotics Institute and <a href="https://dvl.in.tum.de/team/lealtaixe/">Prof. Laura Leal-Taixe</a> (<a href="https://dvl.in.tum.de/">Dynamic Vision and Learning Group</a>) at the Technical University of Munich."""
+#desc = """Hi, I'm Aljosa! I am working towards scaling object detection, segmentation, and tracking models to the open world and learning from raw, unlabeled streams of sensory data.  
+#	<br><br>
+#	I come from the Alpine side of Slovenia. I have obtained my M.Sc. degree at the University of Bonn and Ph.D. at RWTH Aachen University under the supervision of <a href="https://www.vision.rwth-aachen.de/person/1/">Prof. Dr. Bastian Leibe</a>. 
+#	Currently, I am working with <a href="http://www.cs.cmu.edu/~deva/">Prof. Deva Ramanan</a> at the CMU Robotics Institute and <a href="https://dvl.in.tum.de/team/lealtaixe/">Prof. Laura Leal-Taixe</a> (<a href="https://dvl.in.tum.de/">Dynamic Vision and Learning Group</a>) at the Technical University of Munich."""
 css = """https://latex.now.sh/style.min.css""" # Nice LaTeX-style
 pic = """<img src="img/aljosa.png" alt="In Colombia, my fave" width="200" align="left" style="padding:10px;">"""
 links = """<br><b><a href="rss_2020_camready.html">Research Statement</a> 
