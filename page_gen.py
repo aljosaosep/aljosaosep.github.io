6be55077d9f7799f2f19b50a1e599557035af444
aljosa.osep <aljosa.osep@gmail.com>
2025-01-20 11:37:38 -0500

    Reduce clutter, clean comments

--- page_gen.py
+++ page_gen.py
@@ -88,14 +88,9 @@ all_pubs += """</div>"""
 # === Generate head and body ===
 head = """<!DOCTYPE html><html lang="en"><head><title>{}</title><link rel="stylesheet" href={} /></head>""".format(caption, css)
 body = r'<body><h1>{}</h1>{}<p>{}</p>{} <h2>News</h2>{} <h2>Students Supervised</h2>{} <h2>Service</h2>{} <h2>Talks</h2>{} <h2>Teaching</h2> {} <h2>Publications</h2> {} </body></html>'.format(title, pic, desc, links, gen_news, gen_students, gen_service, gen_talks, gen_teaching, all_pubs)
-
-#head = """<!DOCTYPE html><html lang="en"><head><title>{}</title><link rel="stylesheet" href={} /></head>""".format(caption, css)
-#body = r'<body><h2>Publications</h2> {} </body></html>'.format(all_pubs)
-
 final = '{}{}'.format(head, body)
 
 htmlout = "index.html"
 with open(htmlout, "w") as text_file:
 	print (f"Output: {htmlout}")
 	text_file.write(final)
-
